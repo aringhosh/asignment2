e56de8c4f482e7c1d5ae3f04e46d391f34ce4f79
Arin <arin@casestack.io>
2017-10-01 13:26:13 -0700

    finished -- some issue with output format

--- correlate-logs.py
+++ correlate-logs.py
@@ -28,16 +28,17 @@ def getRComponents(t):
 	host, (n, bytes) = t
 	return ('all_hosts', (bytes, n, pow(bytes, 2), pow(n, 2), bytes*n, 1))
 	
-def output_format(comp):
+def calculateR(comp):
 	(host, (sum_x, sum_y, sum_x2, sum_y2, sum_xy, n)) = comp
 	r = (n*sum_xy - sum_x*sum_y)/(math.sqrt(n*sum_x2 - pow(sum_x, 2)) * math.sqrt(n*sum_y2 - pow(sum_y, 2)))
-	return 'r: ', r, 'r2: ',pow(r, 2)
+	return r, pow(r, 2) #('r = ', r), ('r2= ', pow(r, 2))
+
 
 host_bytes = sc.textFile(inputs).map(lambda line: parseline(line)).filter(lambda x: x is not None)
 host_bytes = host_bytes.reduceByKey(add_tuples)
 host_bytes = host_bytes.map(getRComponents)
 host_bytes = host_bytes.reduceByKey(add_tuples)
 
-output_data = host_bytes.map(output_format)
-print(output_data.collect())
-#output_data.saveAsTextFile(output)
+output_data = host_bytes.map(calculateR).map(lambda r: ('r = ', r[0], 'r^2 = ' , r[1]))
+#print(output_data.collect())
+output_data.saveAsTextFile(output)
